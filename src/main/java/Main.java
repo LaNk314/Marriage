3c2b6162f8b56d0c8031371cf2a833cfe2aa3867
LaNk314 <langustek40@gmail.com>
2022-06-28 19:32:03 +0200

    Good to go

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,31 +4,33 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        Population populacja = new Population(5);
-//        for(String man : populacja.man) System.out.print(man +", ");
-//        System.out.print("\n");
-//        for(String woman : populacja.woman) System.out.print(woman + ", ");
-//
-//        System.out.print("\n");
-//        for(int i = 0; i < populacja.size; i++) {
-//            System.out.println((1+i) + ". man preferences: " + populacja.manPref.get(i));
-//            System.out.println((1+i) + ". woman preferences: " + populacja.womanPref.get(i));
-//        }
-
+        Population populacja = new Population(100); //Enter desired size of the population here; folks shall be generated randomly, along with their preferences
         populacja.findStableMatches();
+
+        System.out.println("List of males:");
         int j = 0;
         for(List<String> female : populacja.malesPref) {
             System.out.println((j+1)+". Male preferences: " + female);
             j++;
         }
-        System.out.println("Female preferences: "+populacja.femalesPref);
-        int k = 0;
-        for(String male : populacja.femalesPartner){
-            System.out.println(populacja.female.get(k) + " matched with " + male);
-            k++;
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("List of females:");
+        j = 0;
+        for(List<String> male : populacja.femalesPref) {
+            System.out.println((j+1)+". Female preferences: " + male);
+            j++;
         }
 
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Result:");
 
-
+        j = 0;
+        for(String male : populacja.femalesPartner){
+            System.out.println(populacja.female.get(j) + " matched with " + male);
+            j++;
+        }
     }
 }
--- src/main/java/Population.java
+++ src/main/java/Population.java
@@ -1,16 +1,12 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-
 
 public class Population {
 
     ArrayList<String> male;
     ArrayList<String> female;
     ArrayList<String> femalesPartner;
-    ArrayList<String> unengagedMales;
-
     List<List<String>> malesPref;
     List<List<String>> femalesPref;
     int size;
@@ -21,85 +17,76 @@ public class Population {
         malesPref = new ArrayList<>();
         femalesPref = new ArrayList<>();
         femalesPartner = new ArrayList<>();
-        unengagedMales = new ArrayList<>();
         this.size = size;
         generatePopulation();
         generatePreferences();
         fillPartnersTableList();
     }
 
-    private void fillPartnersTableList() {
-        for(int i = 0; i < size; i++){
-            femalesPartner.add("None");
-        }
-    }
-
-    public void findUnengagedMales(){
-//        unengagedMales.addAll(male);
-//        unengagedMales.removeIf(male -> femalesPartner.contains(male));
-        unengagedMales.addAll(male);
-        for(String male : femalesPartner){
-            if (!male.equals("None")){
-                unengagedMales.remove(male);
-            }
-        }
-    }
-
-    private void generatePreferences() {
-        for(int i = 0; i < size; i++){
-            femalesPref.add(randomize(male));
-            malesPref.add(randomize(female));
-        }
-    }
-
+    //Population setup methods
     private void generatePopulation() {
         for(int i = 0; i < size; i++){
             male.add("M" + (i+1));
             female.add("F" + (i+1));
         }
     }
-
-    public void findStableMatches(){
-//        while(exist male not engaged){
-//            for(male : not engaged males) {
-//                if male not engaged {
-//                    propose till succes
-//                }
-//            }
-//        }
-        findUnengagedMales();
-        while (femalesPartner.contains("None")){
-            for(String male : unengagedMales){
-                int hisIndex = male.indexOf(male);
-                for(String female : malesPref.get(hisIndex)) {
-                    if (propose(male, female)) {
-                        break;
-                    }
-
-                }
-            }
-            findUnengagedMales();
-            System.out.println("loop");
+    private void generatePreferences() {
+        for(int i = 0; i < size; i++){
+            femalesPref.add(randomize(male));
+            malesPref.add(randomize(female));
         }
     }
-
-    public boolean propose(String man, String woman){
-        int herIndex = female.indexOf(woman);
-        String herFiance = femalesPartner.get(herIndex);
-        if(herFiance.equals("None")){
-            femalesPartner.set(herIndex, man);
-            return true;
-        } else if(femalesPref.get(herIndex).indexOf(man) < femalesPref.get(herIndex).indexOf(herFiance)){
-            femalesPartner.set(herIndex, man);
-            return true;
+    private void fillPartnersTableList() {
+        for(int i = 0; i < size; i++){
+            femalesPartner.add("None");
         }
-        else return false;
     }
-
     private ArrayList<String> randomize(ArrayList<String> list){
         ArrayList<String> result = new ArrayList<>();
         result.addAll(list);
         Collections.shuffle(result);
         return result;
     }
+    //End of population setup methods
+
+
+    public void findStableMatches(){
+        while(femalesPartner.contains("None")) {
+            for (String man : male) {
+                if (!femalesPartner.contains(man)) {
+                    findWife(man);
+                }
+            }
+        }
+    }
+    private void findWife(String man){
+        int hisIndex = male.indexOf(man);
+
+        for(String woman : malesPref.get(hisIndex)){
+            int herIndex = female.indexOf(woman);
+            if(propose(man, woman)) {
+                femalesPartner.set(herIndex, man);
+                break;
+            }
+        }
+    }
+    private boolean propose(String man, String woman){
+        int herIndex = female.indexOf(woman);
+
+        if(!femalesPartner.get(herIndex).equals("None")){
+            String herFiance = femalesPartner.get(herIndex);
+            int fiancesPositionInHerPreferences = femalesPref.get(herIndex).indexOf(herFiance);
+            return femalesPref.get(herIndex).indexOf(man) < fiancesPositionInHerPreferences;
+        } else {
+            return true;
+        }
+    }
+
+
+
+
+
+
+
+
 }
